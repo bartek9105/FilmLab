9d21d0d26e5c4dbecac60e318c5f3fe7823be571
bartek9105 <bartek9105@gmail.com>
2019-11-28 12:06:18 +0100

    display film data on new route and load more button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,12 +10,23 @@
           <p>{{ film.title }}</p>
         </router-link>
       </section>
+      <section class="popular">
+        <h2>Popular</h2>
+        <router-link :to="'/film/' + popularFilm.id" class="film-item" v-for="popularFilm in popularFilms.slice(0, filmsToShow)" :key="popularFilm.id">
+          <img :src="'http://image.tmdb.org/t/p/w185'+popularFilm.poster_path" alt="poster">
+          <p>{{ popularFilm.title }}</p>
+        </router-link>
+        <button @click="loadMore">Show more</button>
+      </section>
   </div>
 </template>
 
+
+
 <script>
   const API_KEY = process.env.VUE_APP_API_KEY
-  const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=`
+  const API_URL_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=`
+  const API_URL_POPULAR = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
 
   import SingleFilm from './SingleFilm'
 
@@ -24,18 +35,38 @@
     data(){
       return{
         searchQuery: '',
-        filmDetails: []
+        filmDetails: [],
+        popularFilms: [],
+        filmsToShow: 6
       }
     },
     methods: {
       async getResults(){
         this.filmDetails = [];
-        const results = await fetch(`${API_URL}${this.searchQuery}`)
+        const results = await fetch(`${API_URL_SEARCH}${this.searchQuery}`)
         const data = await results.json();
         data.results.forEach(item => {
           this.filmDetails.push(item)
         })
+      },
+      loadMore(){
+        this.filmsToShow += 6;
       }
+    },
+    async mounted(){
+      const res = await fetch(API_URL_POPULAR)
+      const popularData = await res.json();
+      popularData.results.forEach(item => {
+        this.popularFilms.push(item)
+      })
+
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .popular{
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
--- src/views/SingleFilm.vue
+++ src/views/SingleFilm.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-
+      <div class="single-details" v-for="film in singleFilmDetails">
+          <p>{{ film.title }}</p>
+      </div>
   </div>
 </template>
 
 <script>
+    const API_KEY = process.env.VUE_APP_API_KEY
+
+
     export default {
         data(){
             return{
-
+                singleFilmDetails: [],
+                API_URL: `https://api.themoviedb.org/3/movie/${this.$route.params.id}?api_key=${API_KEY}&language=en-US`
             }
+        },
+        async mounted(){
+            const res = await fetch(this.API_URL)
+            const details = await res.json();
+            this.singleFilmDetails.push(details)
         }
     }
 </script>
